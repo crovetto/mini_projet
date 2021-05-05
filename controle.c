05090c3ecccc4115f4729567c9efb047147fcf4c
crovetto <gianna.crovetto@epfl.ch>
2021-05-05 18:38:46 +0200

    new file : controle

---
+++ controle.c
@@ -0,0 +1,10 @@
+/*
+ * controle.c
+ *
+ *  Created on: 5 May 2021
+ *      Author: giannacrovetto
+ */
+
+
+
+
---
+++ controle.h
@@ -0,0 +1,15 @@
+/*
+ * controle.h
+ *
+ *  Created on: 5 May 2021
+ *      Author: giannacrovetto
+ */
+
+#ifndef CONTROLE_H_
+#define CONTROLE_H_
+
+
+
+
+
+#endif /* CONTROLE_H_ */
